25a2f22266ef2e4826fcd0bd3e53b098f8535879
danila8506 <47233817+danila8506@users.noreply.github.com>
2025-01-18 17:56:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -24,3 +24,13 @@ ANTHROPIC_API_KEY = configdata.get('ANTHROPIC_API_KEY', None)
 YT_DL_DIR = configdata.get('YT_DL_DIR', None)
 YT_DL_URL = configdata.get('YT_DL_URL', None)
 SCHEDULES = configdata.get('SCHEDULES', None)
+{
+    "ALLOWED_USER_NAMES": ["stale_breads"],
+    "OPENAI_API_KEY": "****",
+    "TELEGRAM_TOKEN": "6966957722:AAGPCG3KKljignhOns8MP4XcgatVaalpXIc",
+    "ANTHROPIC_API_KEY": "****",
+    "REPLICATE_API_KEY": "",
+    "YT_DL_DIR": "/var/data/uploads",
+    "YT_DL_URL": "https://somehost.com/uploads",
+    "SCHEDULES": 1
+}
